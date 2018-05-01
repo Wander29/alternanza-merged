fbf6b66cc0c5d3e3e669b6b6eaa789b33d7c6ef4
Andrew-Wyn <terraformer.144@gmail.com>
2018-05-01 16:21:51 +0200

    prova backup

--- server/changep.php
+++ server/changep.php
@@ -49,43 +49,43 @@
     
     if($verifica != null){
         $modifica = mysqli_query($connection, $query_modifica);
-        if($modifica){
+        if(!$modifica){
             $data['success'] = false;
             $data['query'] = "Errore";
-        }
-        $data['success'] = true;
-        $data['query'] = "Password cambiata";
-        
-        try {
-            //Server settings
-            $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host       = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username   = '144.developer.master@gmail.com';
-            $mail->Password   = 'karate1998';
-            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587;                                    // TCP port to connect to
+        } else {
+            $data['success'] = true;
+            $data['query'] = "Password cambiata";
 
-            //Recipients
-            $mail->SetFrom('144.developer.master@gmail.com', "ASL WEB SITE");
-            $mail->AddAddress($nomeut, 'prova mail php');
-            $mail->AddReplyTo('no-reply@mycomp.com','no-reply');
+            try {
+                //Server settings
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host       = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username   = '144.developer.master@gmail.com';
+                $mail->Password   = 'karate1998';
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
 
-            //Attachments
-            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+                //Recipients
+                $mail->SetFrom('144.developer.master@gmail.com', "ASL WEB SITE");
+                $mail->AddAddress($nomeut, 'prova mail php');
+                $mail->AddReplyTo('no-reply@mycomp.com','no-reply');
 
-            //Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                //Attachments
+                //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-            $mail->send();
+                //Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        } catch (Exception $e) {
-            $data["e"] = "maccarena";
+                $mail->send();
 
+            } catch (Exception $e) {
+                $data["e"] = $E -> getMessage();
+            }
         }
         
     } else {
--- server/changepw.php
+++ server/changepw.php
@@ -28,9 +28,41 @@
         if($modifica){
             $data['success'] = false;
             $data['query'] = "Errore";
+        }else {
+            $data['success'] = true;
+            $data['query'] = "Password cambiata";
+
+            try {
+                //Server settings
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host       = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username   = '144.developer.master@gmail.com';
+                $mail->Password   = 'karate1998';
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
+
+                //Recipients
+                $mail->SetFrom('144.developer.master@gmail.com', "ASL WEB SITE");
+                $mail->AddAddress($nomeut, 'prova mail php');
+                $mail->AddReplyTo('no-reply@mycomp.com','no-reply');
+
+                //Attachments
+                //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+                //Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+
+            } catch (Exception $e) {
+                $data["e"] = $E -> getMessage();
+            }
         }
-        $data['success'] = true;
-        $data['query'] = "Password cambiata";
     } else {
         $data['success'] = false;
         $data['query'] = "Errore";
